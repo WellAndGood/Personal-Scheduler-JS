419a9b8499e8b74024342721c7eda808440e1411
F34r Th3 R34per <danielpisani12@hotmail.com>
2021-03-22 21:51:14 -0400

    some fixes and able to clear localStorage

--- script.js
+++ script.js
@@ -1,46 +1,52 @@
-today = moment()
-currentHour = parseInt(moment("12:30 PM", "LT").format("H"))
+// Testing variable declaration for time of day
+// currentHour = parseInt(moment("12:30 PM", "LT").format("H"))
 
 // Displays the time of day
 currentDay = $('#currentDay')
+today = moment()
 currentDay.text( today.format("[Today is] dddd, MMMM Do YYYY"))
 
 var timeBlocks = $('.time-block')
-
-saveButtons = document.querySelectorAll(".saveBtn")
+var saveButtons = document.querySelectorAll(".saveBtn")
+var textAreas = $('.textEntry')
+var lockIcon = $('.lock')
+var clearCalButton = $('.btn-warning')
+var containerDiv = $('container')
 
 $( document ).ready(function() {
     $('.time-block').each(function(index, value) {
         
         var obj = $(value)
         var timeBlock = parseInt(obj.attr("id"))
-        console.log("thisBlock:",  timeBlock)
+        // console.log("thisBlock:",  timeBlock)
         
         var timeBlockMoment = moment(timeBlock, "HH").format("H")
         var timeBlockTextArea = $('.time-block .timeTextarea').eq(index)
-        console.log(index, timeBlockTextArea)
-        //var currentHour = moment().hour();
-        console.log(currentHour)
+        var currentHour = moment().hour();
+        // var currentHour = parseInt(moment("12:30 PM", "LT").format("H"))
+        // console.log(currentHour)
 
-        // ERROR: Does not create any changes in style. Console.logs still work, 
-        // meaning that the time-factored coding is still respected.
-
+        // Time-dependent formatting of textarea parent div
+        // Present Hour 
         if (currentHour < timeBlockMoment) {
-            console.log("this time: " , currentHour)
-            console.log("time block: ", timeBlockMoment)
+            // console.log("this time: " , currentHour)
+            // console.log("time block: ", timeBlockMoment)
             timeBlockTextArea.removeClass("past present")  
             timeBlockTextArea.addClass("future") 
-            console.log(timeBlockMoment + " is in the future of " + currentHour) 
+            // console.log(timeBlockMoment + " is in the future of " + currentHour) 
 
+        // Past hours
         } else if (currentHour > timeBlockMoment) {
-            console.log("this time: " , currentHour)
-            console.log("time block: ", timeBlockMoment)
+            // console.log("this time: " , currentHour)
+            // console.log("time block: ", timeBlockMoment)
             timeBlockTextArea.removeClass("future present")
             timeBlockTextArea.addClass("past")
-            console.log(timeBlockMoment + " is in the past of " + currentHour)
+            // console.log(timeBlockMoment + " is in the past of " + currentHour)
+
+        // Future hours
         } else {
-            console.log("this time: " , currentHour)
-            console.log("time block: ", timeBlockMoment)
+            // console.log("this time: " , currentHour)
+            // console.log("time block: ", timeBlockMoment)
             timeBlockTextArea.removeClass("future past")
             timeBlockTextArea.addClass("present") 
             return
@@ -48,24 +54,54 @@ $( document ).ready(function() {
     });
 })
 
-// finds which element was saved 
-// have to change 'forEach' to 'each' and 'addEventListener' to 'on'
-
 saveButtons.forEach((choice) => {
     choice.addEventListener("click", function(event) {
-        whichButton = $(event.target.dataset.timestamp)
-        findTextArea = $(event.target).parent("div").children(".timeTextarea").children(".textEntry").val()
-        storedItem = localStorage.setItem(whichButton, findTextArea)        
-    })
-})
+        
+        // acts as a pseudo event delegation
+        var whichButton = event.target.parentElement.dataset.timevalue || event.target.parentElement.parentElement.dataset.timevalue;
+        
+        console.log(whichButton)
+        
+        // acts as a pseudo event delegation
+        var findTextArea = $(event.target).parent("div").children(".timeTextarea").children(".textEntry").val() || $(event.target).parent("div").parent("div").children(".timeTextarea").children(".textEntry").val() || ""
+        
+        console.log(findTextArea)
+        
+        var storedItem = localStorage.setItem(whichButton, findTextArea);
+        var lockCheck = event.target.querySelector('.lock') || event.target
+        if (lockCheck.innerText === "🔓") {
+            lockCheck.innerText = "🔐"
+        } else if (lockCheck.innerText === "null") {
+            // console.log("Failure to lock or unlock");
+            return
+        };
+    });
+});
 
-// Gets the local Storage
-$('.schedule #9AMTextArea').val(localStorage.getItem("9AM"))
-$('.schedule #10AMTextArea').val(localStorage.getItem("10AM"))
-$('.schedule #11AMTextArea').val(localStorage.getItem("11AM"))
-$('.schedule #12PMTextArea').val(localStorage.getItem("12PM"))
-$('.schedule #1PMTextArea').val(localStorage.getItem("1PM"))
-$('.schedule #2PMTextArea').val(localStorage.getItem("2PM"))
-$('.schedule #3PMTextArea').val(localStorage.getItem("3PM"))
-$('.schedule #4PMTextArea').val(localStorage.getItem("4PM"))
-$('.schedule #5PMTextArea').val(localStorage.getItem("5PM"))
+
+// Gets the local Storage each time the page loads 
+$('.schedule #9AMTextArea').val(localStorage.getItem("9"))
+$('.schedule #10AMTextArea').val(localStorage.getItem("10"))
+$('.schedule #11AMTextArea').val(localStorage.getItem("11"))
+$('.schedule #12PMTextArea').val(localStorage.getItem("12"))
+$('.schedule #1PMTextArea').val(localStorage.getItem("13"))
+$('.schedule #2PMTextArea').val(localStorage.getItem("14"))
+$('.schedule #3PMTextArea').val(localStorage.getItem("15"))
+$('.schedule #4PMTextArea').val(localStorage.getItem("16"))
+$('.schedule #5PMTextArea').val(localStorage.getItem("17"))
+
+// Undoes the 'lock' feature by typing
+textAreas.on("keyup", function(event) {
+    // console.log(event.key);
+    updateSaveState = $(this).parent().parent().children(2).last().contents();
+    updateSaveState.text("🔓")
+});
+
+// Clears the text areas
+clearCalButton.on("click", containerDiv, function(index) {
+    var clearConfirm = confirm("Are you sure you want to delete your calendar memory?")
+    if (clearConfirm) {
+        localStorage.clear();
+        location.reload();
+    }
+});
